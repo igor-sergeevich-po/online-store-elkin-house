c2e8aac040c618040bada6f7dfcf459b9e3194a0
Igor_PO <89515076779@ya.ru>
2022-12-02 11:02:14 +0300

    add bd & env

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 require('dotenv').config()
 const express = require('express')
-
+const sequelize = require('./db')
 const PORT = process.env.PORT || 5000
 
-// создаём объект вызвав функцию express.
+
 const app = express()
 
-// вешаем функцию и указываем какой порт будет слушать наш сервер, и колбек при ушпешном запуске
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
